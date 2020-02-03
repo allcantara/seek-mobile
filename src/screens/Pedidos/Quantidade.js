351dff1a8671101a932226ccf76f56c71c5c6a3e
Alcântara <alcantara.desenvolvedor@gmail.com>
2020-02-03 14:28:57 -0300

    Update Quantidade.js

--- src/screens/Pedidos/Quantidade.js
+++ src/screens/Pedidos/Quantidade.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
+// Passar o componente novamente para os pedidos pq ele não terá comportamentos próprios.
+// Pensar na possibilidade de isolar todas as lógicas em arquivos para limpar mais o código.
 export default props => <Text style={styles.qtd}>{props.quant}</Text>
 
 const styles = StyleSheet.create({
